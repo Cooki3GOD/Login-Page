3957dd901310e979be26c8209ff04d4a42f85cf0
Lukasz Szczygielski <l_szczygielski@o2.pl>
2024-12-15 16:32:03 +0100

    Added script for the website

---
+++ modernLoginPage2/script.js
@@ -0,0 +1,11 @@
+const registerBtn = document.querySelector("#register");
+const container = document.querySelector(".container");
+const loginBtn = document.querySelector("#login");
+
+registerBtn.addEventListener("click", () => {
+    container.classList.add("active");
+})
+
+loginBtn.addEventListener("click", () => {
+    container.classList.remove("active");
+})
